0f93762d479ec59ec549c0df802d4e0d09ab2e06
Gerrit Holl <gerrit.holl@dwd.de>
2020-04-01 18:38:02 +0200

    Refactoring with less mocking
    Refactor some functionality, have less mocking in unit tests, instead
    use tmpdir.  I should try to avoid mocking my own functions as I would
    be writing passing tests against a non-existing API.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-    Dummy conftest.py for fcitools.
+import pytest
+import os
+import tarfile
 
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    https://pytest.org/latest/plugins.html
-"""
 
-# import pytest
+@pytest.fixture
+def tfs(tmp_path):
+    ptd = tmp_path / "tartest1"
+    ptd.mkdir(exist_ok=True, parents=True)
+    os.chdir(ptd)
+    with open("file1.dat", "wb") as fp:
+        fp.write(b"abcd")
+    with open("file2.dat", "wb") as fp:
+        fp.write(b"efgh")
+    with tarfile.open(ptd / "file.tar", "w") as tf:
+        tf.add("file1.dat")
+        tf.add("file2.dat")
+    with tarfile.open(ptd / "file.tar.gz", "w:gz") as tf:
+        tf.add("file1.dat")
+        tf.add("file2.dat")
+    return (ptd / "file.tar", ptd / "file.tar.gz")
+
+
+@pytest.fixture
+def areas():
+    import pyresample.geometry
+    ad = pyresample.geometry.AreaDefinition(
+            "shrubbery", "it is a good shrubbery", "shrub",
+            {'ellps': 'WGS84', 'lat_0': '0', 'lat_ts': '0', 'lon_0': '0',
+             'no_defs': 'None', 'proj': 'eqc', 'type': 'crs', 'units': 'm',
+             'x_0': 0, 'y_0': 0},
+            750, 300, (2500000, 4000000, 3000000, 40000000))
+    return [ad]

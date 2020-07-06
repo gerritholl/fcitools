6c576566b3af8de7e776c0d4e609d21dcab78b68
Gerrit Holl <gerrit.holl@dwd.de>
2020-07-06 14:29:55 +0200

    More cleanup after moving vis
    Some cleanup after moving the vis stuff to sattools, not sure why
    unittests started failing.

--- src/fcitools/vis.py
+++ src/fcitools/vis.py
@@ -5,6 +5,7 @@ import numpy
 import satpy
 import pathlib
 import xarray
+import sattools.vis
 from . import ioutil
 
 
@@ -58,96 +59,6 @@ def unpack_and_show_testdata(
     areas = ioutil.get_all_areas()
     p = pathlib.Path(path_to_tgz).stem.split(".")[0]  # true stem
 
-    names = show_testdata_from_dir(
-            {str(p) for p in paths},
-            composites,
-            channels,
-            [areas.get(nm) for nm in regions],
-            d_out,
-            fn_out,
-            path_to_coastlines=path_to_coastlines,
-            label=p,
-            show_only_coastlines=show_only_coastlines)
-    return names
-
-
-def show_testdata_from_dir(
-        files,
-        composites,
-        channels,
-        regions,
-        d_out,
-        fn_out,
-        path_to_coastlines=None,
-        label="",
-        show_only_coastlines=False):
-    """Visualise a directory of EUM FCI test data
-
-    From a directory containing EUMETSAT FCI test data, visualise composites
-    and channels for the given regions/areas, possibly adding coastlines.
-
-    Args:
-        files (List[pathlib.Path]):
-            Paths to files
-
-        composites (List[str]):
-            List of composites to be generated
-
-        channels (List[str]):
-            List of channels (datasets) to be generated
-
-        regions (List[str]):
-            List of AreaDefinition objects these shall be generated for.
-            The special region 'native' means no reprojection is applied.
-
-        d_out (pathlib.Path):
-            Path to directory where output files shall be written.
-
-        fn_out (Optional[str]):
-            Pattern of filename in output directory.  Using Python's string
-            formatting syntax, the fields ``area`` and ``dataset`` will be
-            replaced by the region/area and the composite/channel.
-
-        path_to_coastlines (Optional[Str]):
-            If given, directory to use for coastlines.
-
-        label (Optiona[Str]):
-            Additional label to substitute into fn_out.
-
-        show_only_coastlines (Optional[bool]):
-            If true, prepare images showing only coastlines.  Needs at least
-            one channel to be loaded.  Backgrounds will be white, black, and
-            transparent.
-
-    Returns:
-        List of filenames written
-    """
-    L = []
-    sc = satpy.Scene(
-            sensor="fci",
-            filenames=files,
-            reader=["fci_l1c_fdhsi"])
-    if path_to_coastlines is not None:
-        overlay = {"coast_dir": path_to_coastlines, "color": "red"}
-    else:
-        overlay = None
-    sc.load(channels)
-    sc.load(composites)
-    if show_only_coastlines:
-        sc["black"] = xarray.zeros_like(sc[(channels+composites).pop(0)])
-        sc["white"] = xarray.ones_like(sc[(channels+composites).pop(0)])
-        sc["nans"] = xarray.full_like(sc[(channels+composites).pop(0)],
-                                      numpy.nan)
-    for la in regions:
-        ls = sc.resample(la)
-        for dn in ls.keys():
-            fn = pathlib.Path(d_out) / fn_out.format(
-                    area=getattr(la, "area_id", "native"),
-                    dataset=dn.name,
-                    label=label)
-            ls.save_dataset(
-                    dn,
-                    filename=str(fn),
-                    overlay=overlay)
-            L.append(fn)
-    return L
+    return sattools.vis.show(
+        paths, composites, channels, areas, d_out, fn_out, "fci_l1c_fdhsi",
+        path_to_coastlines, label=p, show_only_coastlines=show_only_coastlines)
